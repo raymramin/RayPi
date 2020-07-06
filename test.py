95c1bd379abbbe38a4399953ae23e7c41cd22318
raymramin <66334663+raymramin@users.noreply.github.com>
2020-07-06 16:41:33 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-from pygame import mixer
-import time
+#from pygame import mixer
+#import time
 
-mixer.init()
-alert = mixer.Sound('beep-02.wav')
-time.sleep(5.0)
+#mixer.init()
+#alert = mixer.Sound('beep-02.wav')
+#time.sleep(5.0)
 
-alert.play()
+#alert.play()
+
+from pybeep.pybeep import PyVibrate, PyBeep
+PyBeep().beep()
+PyBeep.beepn(3)
+
 print("Done")
