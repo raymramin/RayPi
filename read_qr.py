49353f551baea368d3d66af95fb1a33b27a2123c
raymramin <66334663+raymramin@users.noreply.github.com>
2020-07-08 22:10:28 -0400

    Add files via upload

--- read_qr.py
+++ read_qr.py
@@ -5,39 +5,56 @@ import time
 import cv2
 
 window_name = "QR Code Scanner"
-data_file = "qrcodedata.txt"
+data_file = "codedata.txt"
 
 # Initialize the video stream
-print("Starting video stream...")
+print("[INFO] Starting stream")
 vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 csv = open(data_file, "w")
-frame = vs.read()
-frame = imutils.resize(frame, width=400)
 
-barcodes = pyzbar.decode(frame)
-cv2.imshow(window_name, frame)
+found = set()
+i = 0
 
-for barcode in barcodes:
-    (x, y, w, h) = barcode.rect
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # make border green
+while True:
+    # Grab frame from threaded video and resize to 400px
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
 
-    barcodeData = barcode.data.decode("utf-8")
-    barcodeType = barcode.type
-    print("barcode data: " + barcodeData)
-    print("barcode type: " + barcodeType)
+    # get all the barcodes from frame
+    barcodes = pyzbar.decode(frame)
 
-    text = "{}".format(barcodeData)
-    cv2.putText(frame, '', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    for barcode in barcodes:
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # make border green
 
-    csv.write("{}\n".format(barcodeData))
-    csv.flush()
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        print("barcode data: " + barcodeData)
+        print("barcode type: " + barcodeType)
 
-    print("Got the barcode data: " + barcodeData)
-    break
-print("Stoping video stream...")
-cv2.destroyWindow(window_name)
+        text = "{}".format(barcodeData)
+        cv2.putText(frame, '', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        if barcodeData not in found:
+            csv.write("{}\n".format(barcodeData))
+            csv.flush()
+
+            found.clear()
+            found.add(barcodeData)
+        else:
+            print("[", i, "] barcode data found: ", barcodeData)
+        i = i + 1
+
+    cv2.imshow(window_name, frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # If user press "Q" then quit the program
+    if key == ord("q"):
+        cv2.destroyWindow(window_name)
+        break
+
+print("[INFO] Cleaning up")
 csv.close()
-print("Done!")
