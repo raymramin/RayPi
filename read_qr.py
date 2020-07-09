e93a9dd33eb1d13eb4d311af4e3124084fb51834
raymramin <66334663+raymramin@users.noreply.github.com>
2020-07-08 22:16:42 -0400

    Add files via upload

--- read_qr.py
+++ read_qr.py
@@ -16,7 +16,6 @@ time.sleep(2.0)
 csv = open(data_file, "w")
 
 found = set()
-i = 0
 
 while True:
     # Grab frame from threaded video and resize to 400px
@@ -45,9 +44,6 @@ while True:
             found.clear()
             found.add(barcodeData)
         break
-        else:
-            print("[", i, "] barcode data found: ", barcodeData)
-        i = i + 1
 
     cv2.imshow(window_name, frame)
     key = cv2.waitKey(1) & 0xFF
